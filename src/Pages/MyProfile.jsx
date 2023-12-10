d3fd650371a570ea47472863127a8d8b624a5427
Preet Sojitra <preet.dev373@gmail.com>
2023-12-10 14:15:44 +0530

    feat: integrate get profile api

--- src/Pages/MyProfile.jsx
+++ src/Pages/MyProfile.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react"
 import { useAuthStore } from "../store/authStore"
 import { useNavigate } from "react-router"
 import { Link } from "react-router-dom"
+import { useProfileStore } from "../store/profileStore"
 
 export const MyProfile = () => {
   const navigate = useNavigate()
@@ -11,15 +12,21 @@ export const MyProfile = () => {
   const accessToken = useAuthStore((state) => state.accessToken)
   const role = useAuthStore((state) => state.role)
 
+  const profile = useProfileStore((state) => state.profile)
+  console.log(profile)
+
   useEffect(() => {
     if (!accessToken) {
-      navigate("/auth/login")
+      navigate("/auth/get-started/login")
     }
   }, [accessToken])
 
   return (
     <div className="min-h-screen bg-gray-100">
-      <div className="flex justify-end p-4">
+      <div className="flex justify-between p-4">
+        <Link to="/home">
+          <button className="font-bold py-2 px-4 rounded">Home</button>
+        </Link>
         <button
           className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
           onClick={handleLogout}
@@ -30,18 +37,21 @@ export const MyProfile = () => {
       <div className="container mx-auto p-8 bg-white rounded shadow-md">
         <div className="text-center mb-8">
           <img
-            src="https://via.placeholder.com/150"
+            // TODO: Change the default avatar to something else
+            src={profile?.avatar || "https://i.pravatar.cc/300"}
             alt="Profile Picture"
             className="rounded-full mx-auto mb-4"
           />
-          <h1 className="text-2xl font-bold">John Doe</h1>
+          <h1 className="text-2xl font-bold">{profile.name}</h1>
           <p className="text-gray-600">Web Developer</p>
         </div>
         <div className="grid grid-cols-2 gap-4">
           <div>
             <h2 className="text-lg font-semibold mb-2">Contact Information</h2>
-            <p>Email: john.doe@example.com</p>
-            <p>Phone: +1 (123) 456-7890</p>
+
+            {profile?.email && <p>Email: {profile.email}</p>}
+
+            {profile?.mobile && <p>Phone: {profile.mobile}</p>}
           </div>
           <div>
             <h2 className="text-lg font-semibold mb-2">Location</h2>
---
+++ src/store/profileStore.js
@@ -0,0 +1,50 @@
+import { create } from "zustand"
+import axios from "axios"
+
+export const useProfileStore = create((set) => {
+  const API_URL = import.meta.env.VITE_API_URL
+
+  let accessToken = localStorage.getItem("accessToken")
+
+  // Access the accesstoken from cookie
+  const cookies = document.cookie.split("; ")
+  const accessTokenCookie = cookies.find((cookie) =>
+    cookie.includes("accessToken")
+  )
+  if (accessTokenCookie) {
+    accessToken = accessTokenCookie.split("=")[1]
+  }
+
+  if (!accessToken) {
+    return {
+      profile: {},
+      fetchProfile: () => {},
+    }
+  }
+
+  const fetchProfile = async () => {
+    try {
+      console.log("Fetching profile")
+      const response = await axios.get(`${API_URL}/profile`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      const {
+        data: { user },
+      } = response
+      //   console.log(response.data)
+
+      set({ profile: user })
+    } catch (error) {
+      console.log("Error fetching profile")
+      console.log(error)
+    }
+  }
+  fetchProfile()
+
+  return {
+    profile: {},
+    fetchProfile: fetchProfile,
+  }
+})
